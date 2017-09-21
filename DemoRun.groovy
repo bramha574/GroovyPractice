d410cd739bc113a5faf7c9948ebeff9c2aafec26
bramha574 <nvbramhavardhan@gmail.com>
2017-09-20 20:18:49 -0400

    loading groovy class

--- DemoRun.groovy
+++ DemoRun.groovy
@@ -1,23 +1,14 @@
 
-    stages {
-        stage('Node Details') {
-            steps {
-                println "${NODE_NAME}"
-            }
-        }
 
-        stage("Release Inputs") {
-            steps {
-                script {
-                    env.RELEASE_SCOPE = input message: 'User input required', ok: 'Release!',
-                            parameters: [choice(name: 'RELEASE_SCOPE', choices: '1\n2\n3', description: 'What is the release scope?')]
-                    string(defaultValue: "TEST", description: 'Product ID?', name: 'userFlag1')
-                    string(defaultValue: "TEST", description: 'Branch ?', name: 'userFlag2')
-                    string(defaultValue: "TEST", description: 'Year ?', name: 'userFlag3')
-                }
-                echo "${env.RELEASE_SCOPE}"
-            }
-        }
-    }
-
-    }//pipeline
+jaas_sensor_step {
+name = "UI"
+pipelinephase = "Build"
+script {
+    env.RELEASE_SCOPE = input message: 'User input required', ok: 'Release!',
+            parameters: [choice(name: 'RELEASE_SCOPE', choices: '1\n2\n3', description: 'What is the release scope?')]
+    string(defaultValue: "TEST", description: 'Product ID?', name: 'userFlag1')
+    string(defaultValue: "TEST", description: 'Branch ?', name: 'userFlag2')
+    string(defaultValue: "TEST", description: 'Year ?', name: 'userFlag3')
+}
+echo "${env.RELEASE_SCOPE}"
+}
--- Jenkinsfile
+++ Jenkinsfile
@@ -1,8 +1,23 @@
 pipeline {
     agent {
       node {
+        label 'linux'
         def rootDir = pwd()
         def example = load "${rootDir}@script/DemoRun.Groovy "
+      }
+
+      stage('Node Details') {
+          steps {
+              println "${NODE_NAME}"
+          }
+      }
+
+      stage("Release Inputs") {
+          steps {
+              DemoRun{
+                Nothing=""
+              }
+          }
       }
     }
 }
